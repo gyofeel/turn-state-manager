173902f9c1f3eb55af838bdac621dffd2a04bb4a
gyofeel <gyofeel@sk.com>
2020-08-29 22:25:00 +0900

    feat: add a TurnGame class and a type constants of events

--- lib/src/TurnGame.ts
+++ lib/src/TurnGame.ts
@@ -1,4 +1,5 @@
 import { Options, EventName, CallbackFunction, CallbackFunctions } from './config/types';
+import { EVENT } from './config/constants';
 
 export class TurnGame {
     public constructor(options: Options) {
@@ -6,29 +7,75 @@ export class TurnGame {
     }
 
     private options: Options = {
-        turnNumber: -1,
         turnTime: 3000,
+        turnIndex: 0,
+        turnNumber: -1,
         totalTime: -1,
         autoTurnover: false,
         loop: false
     }
     private callbackFunctions: CallbackFunctions = {};
+    private turnIndex = 0;
 
+
+    // Initial Game
     private initialize(options: Options) {
         this.setOptions(options);
     }
 
     public setOptions(options: Options) {
-        this.options = options;
+        if (options.turnNumber === 0) {
+            options.turnNumber = -1;
+        }
+        this.options = {
+            ...this.options,
+            ...options
+        }
+        // TODO: call funtions to set a value of the default options.
+        this.setTurnIndex(this.options.turnIndex || 0);
     }
 
+    // Set Event
     public on(eventName: EventName, callback: CallbackFunction) {
         this.callbackFunctions[eventName] = callback;
     }
 
     public emit(eventName: EventName, arg: any) {
+        const { loop, turnNumber } = this.options;
+        
         if (this.callbackFunctions[eventName]) {
             (this.callbackFunctions[eventName]!)(arg);
         }
+        
+        if (eventName === EVENT.NEXT_TURN) {
+            let nextIdx = this.turnIndex + 1;
+            if (turnNumber! > 0 && loop) {
+                nextIdx = nextIdx + 1 >= turnNumber! ? 0 : nextIdx;
+            }
+            this.setTurnIndex(nextIdx);
+        } else if (eventName === EVENT.PREV_TURN) {
+            let prevIdx = this.turnIndex - 1;
+            if (prevIdx < 0) {
+                prevIdx = turnNumber! > 0 && loop ? turnNumber! - 1 : 0;
+            }
+            this.setTurnIndex(prevIdx);
+        }
+        
+        if (turnNumber! > 0) {
+            if (loop) {
+                this.emit(EVENT.COMPLETE, null)
+            }
+            this.emit(EVENT.END, null);
+        }
     }
+
+    // Turn Index
+    private setTurnIndex(index: number) {
+        this.turnIndex = index;
+    }
+
+    public getTurnIndex() {
+        return this.turnIndex;
+    }
+    // Controll Game
 }
---
+++ lib/src/config/constants.ts
@@ -0,0 +1,9 @@
+import { Event } from "./types";
+
+export const EVENT:Event = {
+    START: 'start',
+    PREV_TURN: 'prev-turn',
+    NEXT_TURN: 'next-turn',
+    COMPLETE: 'complete',
+    END: 'end'
+};
