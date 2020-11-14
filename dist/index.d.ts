e703b1817c1b9553e5dceba76449c9fd887175fc
서교필 <gyofeel318@gmail.com>
2020-11-14 13:53:01 +0900

    chrore: remove dist directory

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { TurnStateManager } from './src/TurnStateManager';
-import { EVENT } from './src/config/constants';
-export { TurnStateManager, EVENT };
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EVENT = exports.TurnStateManager = void 0;
-const TurnStateManager_1 = require("./src/TurnStateManager");
-Object.defineProperty(exports, "TurnStateManager", { enumerable: true, get: function () { return TurnStateManager_1.TurnStateManager; } });
-const constants_1 = require("./src/config/constants");
-Object.defineProperty(exports, "EVENT", { enumerable: true, get: function () { return constants_1.EVENT; } });
-//# sourceMappingURL=index.js.map
