2ee86dd9db85b7e2ce193629ee2d92aa9d8f9f62
gyofeel <gyofeel318@gmail.com>
2020-12-01 22:52:12 +0900

    fix: bug fix

--- lib/src/TurnGame.ts
+++ lib/src/TurnGame.ts
@@ -140,7 +140,7 @@ export class TurnGame {
             return;
         }
 
-        if (turnNumber! > 0 && (this.turnIndex + 1) >= turnNumber!) {
+        if (turnNumber! > 0 && (this.turnIndex + 1) > turnNumber!) {
             if (loop) {
                 this.emitCompleteEvent();
             } else {
